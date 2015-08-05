5863410fe2df1a789a9e719e49c2027b0fee071d
Thom Jordan <viresh53@gmail.com>
2015-08-05 18:34:59 -0400

    Initial Commit

---
+++ MidiToolbox/MidiToolbox.h
@@ -0,0 +1,19 @@
+//
+//  MidiToolbox.h
+//  MidiToolbox
+//
+//  Created by Thom Jordan on 8/5/15.
+//  Copyright (c) 2015 Thom Jordan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MidiToolbox.
+FOUNDATION_EXPORT double MidiToolboxVersionNumber;
+
+//! Project version string for MidiToolbox.
+FOUNDATION_EXPORT const unsigned char MidiToolboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MidiToolbox/PublicHeader.h>
+
+
